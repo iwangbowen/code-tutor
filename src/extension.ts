cf0df10e0d9a67ef4f028bbd0128f4df87d853e3
Bowen Wang <iwangbowen@gmail.com>
2024-10-19 11:07:45 +0800

    添加代码导师聊天处理程序，定义基本提示和模型选择器

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,42 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const BASE_PROMPT =
+  'You are a helpful code tutor. You can answer anything';
+
+const MODEL_SELECTOR: vscode.LanguageModelChatSelector = {
+	vendor: 'copilot',
+	family: 'gpt-4o'
+};
+
+// define a chat handler
+const handler: vscode.ChatRequestHandler = async (
+	request: vscode.ChatRequest,
+	context: vscode.ChatContext,
+	stream: vscode.ChatResponseStream,
+	token: vscode.CancellationToken
+  ) => {
+	let prompt = BASE_PROMPT;
+
+	const [model] = await vscode.lm.selectChatModels(MODEL_SELECTOR);
+
+	if (model) {
+		const messages = [vscode.LanguageModelChatMessage.User(prompt)];
+
+		messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
+
+		const chatResponse = await model.sendRequest(messages, {}, token);
+
+		for await (const fragment of chatResponse.text) {
+			stream.markdown(fragment);
+		}
+	}
+
+	return;
+  };
+
+const tutor = vscode.chat.createChatParticipant('chat-tutorial.code-tutor', handler);
+
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-tutor" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-tutor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Code Tutor!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
